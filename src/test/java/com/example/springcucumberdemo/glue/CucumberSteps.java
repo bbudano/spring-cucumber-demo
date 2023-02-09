58500354dc54fe3acca1f7b7f3a6e3b5bc12da20
Bernard Budano <bbudano@gmail.com>
2023-02-09 15:59:11 +0100

    Client can create a new guitar feature updated

--- src/test/java/com/example/springcucumberdemo/glue/CucumberSteps.java
+++ src/test/java/com/example/springcucumberdemo/glue/CucumberSteps.java
@@ -2,6 +2,7 @@ package com.example.springcucumberdemo.glue;
 
 import com.example.springcucumberdemo.model.Guitar;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
@@ -13,24 +14,30 @@ import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 
-public class CucumberDefinitions {
+public class CucumberSteps {
 
     @LocalServerPort
     String port;
+
+    private HttpEntity<Guitar> requestEntity;
 
     private ResponseEntity<Guitar> response;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Given("the client wants to create a guitar with manufacturer {string} and model {string}" +
+            " and finish {string} and serial number {string} and amount {bigdecimal}")
+    public void givenClientWantsToCreateGuitar(String manufacturer, String model, String finish, String serialNumber, BigDecimal amount) {
+        var guitar = new Guitar(null, manufacturer, model, finish, serialNumber, amount);
+        requestEntity = new HttpEntity<>(guitar);
+    }
+
     @When("the client calls endpoint {string}")
     public void whenClientCallsEndpoint(String path) {
-        var guitar = new Guitar(null, "Chapman", "ML2 Pro", "River Styx Black",
-                "12345", new BigDecimal("1245.00"));
-
         this.response = restTemplate.exchange(
                 "http://localhost:" + this.port + path,
                 HttpMethod.POST,
-                new HttpEntity<>(guitar),
+                requestEntity,
                 Guitar.class);
     }
 
@@ -42,6 +49,7 @@ public class CucumberDefinitions {
     @And("response body contains manufacturer {string} and model {string} and finish {string} and serial number {string} and amount {bigdecimal}")
     public void responseBodyMatches(String manufacturer, String model, String finish, String serialNumber, BigDecimal amount) {
         Assertions.assertNotNull(response.getBody());
+        Assertions.assertNotNull(response.getBody().id());
         Assertions.assertEquals(manufacturer, response.getBody().manufacturer());
         Assertions.assertEquals(model, response.getBody().model());
         Assertions.assertEquals(finish, response.getBody().finish());
